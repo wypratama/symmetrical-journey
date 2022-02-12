3c86e05320447906bdccffcce20e1edb8f73ddc3
wypratama <wicaksanapratama@gmail.com>
2022-02-12 16:30:29 +0700

    feat(init): provide basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './assets/styles/App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Home, MovieDetail } from './views';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='detail' element={<MovieDetail />} />
+      </Routes>
+      <nav>
+        <Link to='/'>Home</Link>
+      </nav>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import './assets/styles/reset.css';
 import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
