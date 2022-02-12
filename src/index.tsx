748e5ddf9652caa5b26dee5aa4695d9b34e1f109
wypratama <wicaksanapratama@gmail.com>
2022-02-12 15:34:26 +0700

    refactor: group assets to one folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/styles/reset.css';
+import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
